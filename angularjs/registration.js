89c5cbc62f2d623ffe13db0a4d9a0ac7aea9b84a
Henrik Johansson <hencjo@gmail.com>
2013-07-02 20:44:43 +0200

    Sorted highlight of tickets.

--- angularjs/registration.js
+++ angularjs/registration.js
@@ -1,20 +1,19 @@
 function RegistrationController($scope, $resource, $window) {
-	var RegistrationResource = $resource("http://backoffice.brewingagile.org/api/registration/1/", {});
-	// var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
+	// var RegistrationResource = $resource("http://backoffice.brewingagile.org/api/registration/1/", {});
+	var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
 
 	$scope.r = {
 		participantName: "",
 		participantEmail: "",
 		billingCompany: "",
 		billingAddress: "",
-		dietaryRequirements: false,
-		workshop: false
+		dietaryRequirements: "",
+		ticket: ""
 	};
 
-	$scope.submit = function() {
-		RegistrationResource.save($scope.r, function(result) {
-			$window.alert("We love popups. And ponies.")
-		});
+	$scope.highlightConference = function() {
+		if ($scope.ticket === 'conference') return "checked";
+		return ""
 	};
 }
 
