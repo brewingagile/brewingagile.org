b4021c3b506fbb2a8d06ca7d623eb52acfb084dd
Henrik Johansson <hencjo@gmail.com>
2013-07-02 20:45:09 +0200

    Accidentaly commited test resource.

--- angularjs/registration.js
+++ angularjs/registration.js
@@ -1,6 +1,6 @@
 function RegistrationController($scope, $resource, $window) {
-	// var RegistrationResource = $resource("http://backoffice.brewingagile.org/api/registration/1/", {});
-	var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
+	var RegistrationResource = $resource("http://backoffice.brewingagile.org/api/registration/1/", {});
+	// var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
 
 	$scope.r = {
 		participantName: "",
