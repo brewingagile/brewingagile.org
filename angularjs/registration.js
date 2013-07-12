3e331702aea37fedfe0951fb7495446cdd257c67
Henrik Johansson <hencjo@gmail.com>
2013-07-12 22:49:31 +0200

    Sorted feedback on registration.

--- angularjs/registration.js
+++ angularjs/registration.js
@@ -1,6 +1,8 @@
-function RegistrationController($scope, $resource, $window) {
+function RegistrationController($scope, $resource, $window, $timeout) {
 	var RegistrationResource = $resource("http://backoffice.brewingagile.org/api/registration/1/", {});
 	// var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
+
+	$scope.lastRegisteredName = "";
 
 	$scope.r = {
 		participantName: "",
@@ -11,11 +13,25 @@ function RegistrationController($scope, $resource, $window) {
 		workshop: false
 	};
 
+	$scope.reset = function() {	
+		$scope.showSuccess = false;
+		$timeout(function() {
+			document.getElementById("inputParticipantName").focus(); //this is not the angular way, I know.
+		});
+	};
+
+	$scope.reset();
+
 	$scope.submit = function() {
+		$scope.showSuccess = false;
+		$scope.showError = false;
 		RegistrationResource.save($scope.r, function(result) {
-			$window.alert("We love popups. And ponies.")
+			$scope.lastRegisteredName = $scope.r.participantName;
+			$scope.showSuccess = true;
+		}, function(response) { 
+			$scope.showError = true;
 		});
 	};
 }
 
-RegistrationController.$inject = ['$scope', '$resource', '$window'];
+RegistrationController.$inject = ['$scope', '$resource', '$window', '$timeout'];
