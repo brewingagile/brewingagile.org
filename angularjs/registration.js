46430861a9415065fe59adbe4b8105126abfd553
Henrik Johansson <hencjo@gmail.com>
2014-05-15 21:31:14 +0200

    badIE in AngularJS-controller.

--- angularjs/registration.js
+++ angularjs/registration.js
@@ -1,6 +1,7 @@
-function RegistrationController($scope, $resource, $window, $timeout) {
+function RegistrationController($scope, $resource, $window, $timeout, IE) {
 	var RegistrationResource = $resource("https://backoffice.brewingagile.org/api/registration/1/", {});
-	// var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
+//	var RegistrationResource = $resource("http://localhost\\:9080/ba-backoffice/api/registration/1/", {});
+  $scope.badIE = (IE.isIE && IE.version < 10);
 
 	$scope.lastRegisteredName = "";
 
